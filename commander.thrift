ba5ba98036e39c1e0e7ce314b547e256d42cb055
lr-erics <lr.erics@gmail.com>
2020-05-14 17:05:01 +0800

    Update commander.thrift

--- commander.thrift
+++ commander.thrift
@@ -44,7 +44,7 @@ struct HoldingItem {
 2: StockType type        
 3: double market_value   // 总市值
 4: i32 shares            // 总持仓量
-5. map<string, i32> holding_details  // 策略id->持仓量
+5. map<string, i32> (cpp.template = "std::unordered_map") holding_details  // 策略id->持仓量
 }
 
 // 策略关联交易账户的持仓信息
@@ -56,7 +56,7 @@ struct FundAccountInfo {
 }
 
 struct StrategyObject {
-1: map<string, list<FundAccountInfo>> details; 
+1: map<string, list<FundAccountInfo>> (cpp.template = "std::unordered_map") details; 
 }
 
 struct CommanderReq{

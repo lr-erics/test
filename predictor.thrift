da5297bc667f050f6b7a729721a8c97a4fdefaff
lr-erics <lr.erics@gmail.com>
2020-07-13 11:59:45 +0800

    Update predictor.thrift

--- predictor.thrift
+++ predictor.thrift
@@ -3,12 +3,18 @@ namespace py hqth.trader
 
 cpp_include "<unordered_map>"
 
-struct PredictorReq {
-1: string uuid          //用于标示一次唯一请求，可追溯问题。
-2: string sender_ip     //请求方ip，方便追溯问题
-3: bool is_ready        //因子数据是否准备好。
-4: i64 timestamp        //当前最新可用的因子集合的时间
-}
+ enum ReqType {
+ NORMAL = 0,    //正常买卖请求
+ SPSL   = 1,    //止盈止损例行请求 stop profile and stop loss
+ }
+ 
+ struct PredictorReq {
+ 1: string uuid          //用于标示一次唯一请求，可追溯问题。
+ 2: string sender_ip     //请求方ip，方便追溯问题
+ 3: bool is_ready        //因子数据是否准备好。
+ 4: i64 timestamp        //当前最新可用的因子集合的时间
+ 5: ReqType type         //
+ }
 
 //struct PredictorRsp{
 //1: string uuid
